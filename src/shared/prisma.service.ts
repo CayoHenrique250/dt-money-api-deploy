ac7c281703d8ea2feb8d9ab5cc5b67f3c24f34e0
Cayo Henrique <cayoh250@gmail.com>
2026-05-08 01:09:44 -0300

    fix: vercel corrections

--- src/shared/prisma.service.ts
+++ src/shared/prisma.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
     const connectionString = process.env.DATABASE_URL;
-    const adapter = new PrismaPg( { connectionString });
-    super( { adapter});
+    const adapter = new PrismaPg({ connectionString });
+    super({ adapter });
   }
 }
 /*export class PrismaService extends PrismaClient {
